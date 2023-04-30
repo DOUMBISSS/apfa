7311af33a747aeb8b8fdb35e03f086b5bb789e53
DOUMBIA FODE <104175220+DOUMBISSS@users.noreply.github.com>
2023-04-30 00:58:43 +0000

    ma

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -12,13 +12,103 @@ export default function Accueil () {
       <div>
                <Navbar/>
         <div className='container'>
-          <div className="container">
-            <h2 className='title__categorie'> Les produits les plus populaires</h2>
-            <div className='more__order__res'>
+          <div className='section__searching'>
+            <div className='section__searching__content'>
+            <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label for="floatingInput">Une formation , un métier , un mot clé</label>
+              </div>
+              <button className='btn__details__search'>Afficher toutes nos formations</button>
+            </div>
           </div>
-          
+
+          <div className='section__presentation'>
+            <div className='section__presentation__content'>
+                <div className='cards'>
+                    <div className='cards__header'>
+                        <h6>NOTRE OFFRE PARTICULIER</h6>
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <div className='cards__body'>
+                    <p>Apprendre, se former, réfléchir à son projet pro, se reconvertir</p>
+                    <div className='cards__descriptions'>
+                      <p>Formez-vous à distance</p>
+                      <p>Devenez agent de restauration Buffalo Grill</p>
+                      <p>Devenez Technicien vendeur chez Decathlon</p>
+                      <p>Devenez Assistante de vie avec Oui Care</p>
+                    </div>
+                    <div className='btn__container'>
+                      <button className='btn__more'>Découvrir plus</button>
+                    </div>
+
+                    </div>
+                </div>
+
+                <div className='cards'>
+                <div className='cards__header'>
+                        <h6>NOS FORMATIONS EN ALTERNANCE</h6>
+                        <i class="fa-solid fa-chart-simple"></i>
+                    </div>
+                    <div className='cards__body'>
+                    <p>Nos formations prêtes a l'emploi sur plus de 200 métiers</p>
+                    <div className='cards__descriptions'>
+                      <p>Nos formations prêtes à l'emploi </p>
+                      <p> Trouvez votre entreprise</p>
+                      <p> Se former a l'Afpa</p>
+                      <p>Le choix de l'alternance</p>
+                    </div>
+                    <div className='btn__container'>
+                      <button className='btn__more'>Découvrir plus</button>
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div className='cards'>
+                <div className='cards__header'>
+                        <h6>RECRUTEMENT EN ALTERNANCE</h6>
+                        <i class="fa-solid fa-users"></i>
+                    </div>
+                    <div className='cards__body'>
+                    <p>Un accompagnement complet pour former et recruter vos alternants</p>
+                    <div className='cards__descriptions'>
+                      <p>L'alternance avec l'Afpa</p>
+                      <p> Un recrutement encadré et maîtrisé</p>
+                      <p> Un service clé en main </p>
+                      <p>Un formation en tutorial</p>
+                    </div>
+                    <div className='btn__container'>
+                      <button className='btn__more'>Découvrir plus</button>
+                    </div>
+
+                    </div>
+
+                </div>
+            </div>
 
           </div>
+
+          <div className='section__filter'>
+    
+
+          </div>
+
+          <div className='section__info'>
+              <h2 className='section__info__header'> Fill info</h2>
+              <div className='section__info__container'>
+                  <div className='section__info__content'>
+
+                  </div>
+                  <div className='section__info__content'>
+
+                  </div>
+                  <div className='section__info__content'>
+
+                  </div>
+              </div>
+          </div>
+
            </div>
           <Footer/>
         </div>
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,162 +1,72 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import {Link, useNavigate} from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import {Link} from 'react-router-dom';
 import NavLogin from '../components/NavLogin';
 
 
-function Navbar ({search,setSearch}) {
-  const [user,setUser] = useState("");
+function Navbar ({}) {
   const [nav , setNav] = useState(false);
-  const [cartShop , setCartShop] = useState(false);
   const [display,setDisplay]=useState(false);
-  const [success, setSuccess] = useState(false);
-  const navigate = useNavigate();
+  const [notif,setNotif]=useState(false)
+
 
     const openLog = ()=>{
       setNav(true)
     }
-
-    const showCart =()=>{
-      setCartShop (true)
-  }
-
-  const showProfil =()=>{
-    setDisplay(true)
-  }
-  const closeProfil =()=>{
-    setDisplay(false)
-  }
   const showMenu = () =>{
     setDisplay(true)
 }
-const affi = ()=> {
+const afficher = ()=> {
   setDisplay (false)
 }
 
-  const logout =()=>{
-    localStorage.clear();
-    navigate('/');
+  const showNotifications = () => {
+    setNotif(true)
   }
-  const auth = localStorage.getItem("user");
-
-  const handleSearch = (e)=>{
-    setSearch(e.target.value)
-  }
-  const searching =(e)=>{
-    navigate('/searching');
-    setSearch(e.target.value)
-  }
-
     return (
       <div>
       <header>
 
-        { auth ? (
         <div className='navbar'>
           <div className="navbar--left">
           <div className='icon--menu' onClick={showMenu}>
                <i className="fa-solid fa-bars"></i>
                </div>
-            <div className="navbar--logo">
-              <Link to="/"> <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt=""/></Link>
+               <div className="navbar--logo">
+              <img src="https://expertisefrance.fr/documents/20182/248002/Logo+Afpa/ca89131e-b08f-9308-df84-614b9fd403b2?t=1676650683756" alt="" />
             </div>
           </div>
           <div className="navbar--center">
             <div className="navbar--center--content">
-              <Link className='liste' to="/">Accueil</Link>
-              <Link className='liste' to='/men'> Homme</Link>
-              <Link className='liste' to='/women'> Femme</Link>
-              <Link className='liste' to='/child'>Enfant</Link>
-              <Link className='liste' to='/accessoires'>Accessoires</Link>
-              <div className="col-3 col-md-6">
-                <form class="d-flex" role="search">
-                <input class="form-control me-3" type="search" placeholder="Cherchez un produit , une marque ou une categorie..." aria-label="Search" onChange={handleSearch} value={search}/>
-                <button class="btn btn-outline-success" onClick={searching}>Search</button>
-              </form>
-              </div>
+            <Link className='liste' to="/Accueil">Accueil</Link>
+              <Link className='liste' to='/Accueil'> Partenaire</Link>
+              <Link className='liste' to='/Accueil'> Entreprise</Link>
+              <Link className='liste' to='/Accueil'>Nos Formations</Link>
+              <Link className='liste' to='/Accueil'> Actualités</Link>
+              <Link className='liste' to='/Accueil'> Groupe Apfa</Link>
             </div>
-            <div className='logo--resp'>
-              <div className="navbar--logo--resp">
-                <Link to="/"> <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt=""/></Link>
-              </div>
-            </div>
-               
           </div>
 
         
           <div className="navbar--right">
           <div className="navbar--right--content">
-                       <NavLogin nav={nav} setNav={setNav}/>
-                       <Link className='liste' ><button onClick={openLog} className="btn--connexion"><i className="fa-solid fa-user"></i> Mon profil </button></Link>
+                            {/* <div className='nav--items'>
+                              <i class="fa-solid fa-bell" onClick={showNotifications}></i>
+                            </div> */}
+                            <NavLogin nav={nav} setNav={setNav}/>
+                            <button onClick={openLog} className="btn--connexion"> <i class="fa-solid fa-user"></i> Mon profil </button>
                   </div>
           </div>
          </div>
-        ) : (
-          <div className='navbar'>
-          <div className="navbar--left">
-          <div className='icon--menu' onClick={showMenu}>
-               <i className="fa-solid fa-bars"></i>
-               </div>
-            <div className="navbar--logo">
-              <Link to="/"> <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt=""/></Link>
-            </div>
-          </div>
-          <div className="navbar--center">
-            <div className="navbar--center--content">
-              <Link className='liste' to="/">Accueil</Link>
-              <Link className='liste' to='/men'> Homme</Link>
-              <Link className='liste' to='/women'> Femme</Link>
-              <Link className='liste' to='/child'>Enfant</Link>
-              <Link className='liste' to='/accessoires'>Accessoires</Link>
-              <div className="col-3 col-md-6">
-                <form class="d-flex" role="search">
-                <input class="form-control me-3" type="search" placeholder="Cherchez un produit , une marque ou une categorie..." aria-label="Search" onChange={handleSearch}/>
-                <button class="btn btn-outline-success" onClick={searching}>Search</button>
-              </form>
-              </div>
-            </div>
-            <div className='logo--resp'>
-              <div className="navbar--logo--resp">
-                <Link to="/"> <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt=""/></Link>
-              </div>
-            </div>
-          </div>
-          <div className="navbar--right">
-          <div className="navbar--right--content">
-                       <Link to='/loginPage'><button className="btn--connexion"> <i class="fa-solid fa-user"></i>  Connexion</button></Link>
-                  </div>
-          </div>
-         </div>
-        )}
 
         <div className={display ? "menu show--menu" : "menu"}>
-                    <div className='sidebar--menu'>
-                        <div className='btn--close--sidebar' onClick={affi}>
+                    <div className='sidebar--menu' onClick={afficher}>
+                        <div className='btn--close--sidebar'>
                             <i className="fa-solid fa-xmark"></i>
                         </div>
                         <div className='menu__content'>
                           <div className='menu__content__liste'>
                             <Link className='liste' to="/">Accueil</Link>
-                          </div>
-                          <div className='menu__content__liste'>
-                            <Link className='liste' to='/men'> Homme</Link>
-                          </div>
-                          <div className='menu__content__liste'>
-                            <Link className='liste' to='/women'> Femme</Link>
-                          </div>
-                            <div className='menu__content__liste'>
-                            <Link className='liste' to='/child'>Enfant</Link>
-                            </div>
-                            <div className='menu__content__liste'>
-                            <Link className='liste' to='/accessoires'>Accessoires</Link>
-                            </div>
-                          <div className="col-12 col-md-12">
-                            <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Cherchez un produit , une marque ou une categorie..." aria-label="Search"/>
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                          </form>
                           </div>
                         </div>
                         </div>
